7f16cca17f28b01283549fa3f1a5b09d2a393c4a
sarita <sarita.chetak@gmail.com>
2019-08-11 13:48:56 +0100

    sonu

---
+++ src/ProgramMultiplication/Mul.java
@@ -0,0 +1,15 @@
+package ProgramMultiplication;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Mul {
+    public static void main(String[] args) {
+        int a,b,result;
+        a=100;
+        b=30;
+        result=a*b;
+        System.out.println("result=" +result);
+
+
+    }
+}
